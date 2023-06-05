02748186bfaed8aa1833df12a56d4be1f9519788
Maciej Garbacz <furn20102@gmail.com>
2023-06-05 13:13:01 +0200

    Added tab remaps

--- lua/garbacz/remap.lua
+++ lua/garbacz/remap.lua
@@ -13,6 +13,16 @@ vim.keymap.set("n", "<leader>tc", ":terminal<CR>i")
 vim.keymap.set("n", "<leader>tv", "<C-w>v<C-w>l:terminal<CR>i")
 vim.keymap.set("n", "<leader>ts", "<C-w>s<C-w>j:terminal<CR>i")
 
+vim.keymap.set("n", "<leader>1", "1gt")
+vim.keymap.set("n", "<leader>2", "2gt")
+vim.keymap.set("n", "<leader>3", "3gt")
+vim.keymap.set("n", "<leader>4", "4gt")
+vim.keymap.set("n", "<leader>5", "5gt")
+vim.keymap.set("n", "<leader>6", "6gt")
+vim.keymap.set("n", "<leader>7", "7gt")
+vim.keymap.set("n", "<leader>8", "8gt")
+vim.keymap.set("n", "<leader>9", "9gt")
+
 vim.keymap.set("v", "d", "\"_d")
 vim.keymap.set("v", "D", "\"_D")
 
